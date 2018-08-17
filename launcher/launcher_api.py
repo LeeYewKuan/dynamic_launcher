47144837c7e39d93adb7db656f2857a8dd7739df
LeeYewKuan <liyaoguang45@163.com>
2018-08-18 00:07:42 +0800

    完成UI数据接口的初步更改, 解决retrofit请求key=value的问题, 去人requet请求域

--- launcher/launcher_api.py
+++ launcher/launcher_api.py
@@ -11,12 +11,26 @@ from launcher.model.models import db
 from launcher.model.models import Navigation
 # 导入json 工具类
 from flask import jsonify
+# 导入os类
+import os
+# 导入flask请求对象，获取请求的参数
+from flask import request
+# 导入json
+import json
 
 # 设置蓝图别名
 blue_launcher = Blueprint('launcher', __name__)
 
+# 常用的请求模式组合
+request_methods = ['GET', 'POST']
 
-@blue_launcher.route("/navigation_list", methods=["GET", "POST"])
+# 文件的根目录, __file__ refers to the file settings.py
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+# 静态json文件存放的具体路径
+APP_STATIC_JSON = os.path.join(APP_ROOT, 'static/json')
+
+
+@blue_launcher.route("/navigation_list", methods=request_methods)
 def get_navigation_list():
     nav_list = Navigation.query.filter().all()
     data_list = []
@@ -24,9 +38,6 @@ def get_navigation_list():
         result = item.to_json()
         result.pop('id')
         data_list.append(result)
-
-    print(nav_list)
-    print(type(nav_list))
     task = {
         "ret": "0",
         "message": "成功",
@@ -35,12 +46,27 @@ def get_navigation_list():
     return jsonify(task), 200
 
 
-@blue_launcher.route("/app_config", methods=["GET", "POST"])
+@blue_launcher.route("/app_config", methods=request_methods)
 def app_config():
     task = {
         "ret": "0",
         "message": "成功",
-        "link": "http://pdi6uq9ul.bkt.clouddn.com/logo.png"
+        "data": {
+            "logo": "http://pdi6uq9ul.bkt.clouddn.com/logo.png"
+        }
     }
     return jsonify(task), 200
 
+
+@blue_launcher.route("/get_panel", methods=request_methods)
+def get_panel():
+    str_params = str(request.get_data(), encoding='utf-8')
+    print(str_params)
+    panel_id = str_params.split("=")[1]
+    print(panel_id)
+    file_name = 'featured_%s.txt' % 1001
+    path = os.path.join(APP_STATIC_JSON, file_name)
+    print(path)
+    with open(path, 'r') as f:
+        result = f.read()
+    return result.encode(), 200
--- runserver.py
+++ runserver.py
@@ -2,7 +2,6 @@
 # -8- encoding=utf8 -8*
 
 from launcher import app
-import os
 # 导入蓝图类
 from launcher.launcher_api import blue_launcher
 
@@ -10,25 +9,10 @@ from launcher.launcher_api import blue_launcher
 host = '192.168.101.105'
 # 端口号
 port = 8080
-# 常用的请求模式组合
-request_methods = ['GET', 'POST']
 
-# 文件的根目录, __file__ refers to the file settings.py
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-# 静态json文件存放的具体路径
-APP_STATIC_JSON = os.path.join(APP_ROOT, 'launcher/static/json')
 
 # 注册模块蓝图,饼指定url 前缀
 app.register_blueprint(blue_launcher, url_prefix='/launcher')
-
-
-@app.route('/index', methods=request_methods)
-def index():
-    path = os.path.join(APP_STATIC_JSON, 'featured.txt')
-    print(path)
-    with open(path, 'r') as f:
-        result = f.read()
-    return result.encode()
 
 
 if __name__ == '__main__':
